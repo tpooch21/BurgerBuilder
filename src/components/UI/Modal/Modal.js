9f02d57a8bfb3ad593a185f1975f1af62c15793e
Trevor Puccini <trevorpuccini21@gmail.com>
2020-09-02 19:48:23 -0700

    Add useEffect to Modal so it only renders when show prop changes

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import classes from './Modal.module.css';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = props => (
-  <Aux>
-    <Backdrop
-      show={props.show}
-      close={props.close} />
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}>
-      {props.children}
-    </div>
-  </Aux>
-);
+const Modal = props => {
+  useEffect(() => {
+    console.log('[Modal] updating');
+  }, [props.show]);
 
-export default modal;
+  return (
+    <Aux>
+      <Backdrop
+        show={props.show}
+        close={props.close} />
+      <div
+        className={classes.Modal}
+        style={{
+          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+          opacity: props.show ? '1' : '0'
+        }}>
+        {props.children}
+      </div>
+    </Aux>
+  );
+};
+
+export default Modal;
